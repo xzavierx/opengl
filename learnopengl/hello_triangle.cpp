88e8457b1b6fd52cca83f39defdb17d1ca7cbb29
lufei <lufei@abc.com>
2024-01-25 15:53:49 +0800

    add textures

--- learnopengl/hello_triangle.cpp
+++ learnopengl/hello_triangle.cpp
@@ -82,9 +82,9 @@ int main() {
   glDeleteShader(fragmentShader);
 
   float vertices[] = {
-    -0.5f, -0.5f, 0.0f, //left
-    0.5f,  -0.5f, 0.0f, // right
-    0.0f,  0.5f,  0.0f  // top
+    -0.75f, -0.75f, -1.0f, //left
+    0.75f,  -0.75f, -1.0f, // right
+    0.0f,  0.75f,  -1.0f  // top
   };
   unsigned int VBO, VAO;
   glGenVertexArrays(1, &VAO);
